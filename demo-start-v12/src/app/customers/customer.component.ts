b46f3972489c2af2ebeac8b8755f1627338b48d9
AdamJamesWatkins <adamjwatkins@gmail.com>
2023-04-24 22:41:57 +0100

    Added a debounceTime to the emailControl validation

--- demo-start-v12/src/app/customers/customer.component.ts
+++ demo-start-v12/src/app/customers/customer.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 import { Customer } from './customer';
 
+import { debounceTime } from 'rxjs/operators';
+
 function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
   const emailControl = c.get('email');
   const confirmEmailControl = c.get('confirmEmail');
@@ -62,7 +64,9 @@ export class CustomerComponent implements OnInit {
     );
 
     const emailControl = this.customerForm.get('emailGroup.email');
-    emailControl?.valueChanges.subscribe(
+    emailControl?.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
       value => this.setMessage(emailControl)
     );
   }
